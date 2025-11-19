9bffd31fe6d0045a0b3c11e0d8ca77e82ca34fdc
Kalpataru Behera <enigmatic.coder.075@gmail.com>
2025-11-19 17:42:13 +0530

    added new files

---
+++ Operator overloading/unary2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Distance {
+    int feet;
+    int inch;
+
+    public: 
+    Distance(int f, int i) {
+        feet = f;
+        inch = i;
+    }
+
+    void operator- () {
+        feet--;
+        inch--;
+        cout<< feet<<" "<<inch;
+
+        
+    }
+
+
+};
+
+int main() {
+    Distance d1(10, 20);
+
+    -d1;
+    
+    return 0;
+}
